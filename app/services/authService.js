31c84a20734a59bc67674aed06fcdbd70f58d6a3
randiranirman <randiranirman20011210@gmail.com>
2025-08-25 12:05:47 +0530

    fixed the sign in  functionality

--- app/services/authService.js
+++ app/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
- const API_URL =  " http://localhost:8080/api/auth"
+ const API_URL =  " http://192.168.8.108:8080/api/auth"
 
 
 
@@ -9,6 +9,7 @@ import axios from "axios"
 
     try  { 
       const response  = await axios.post(`${API_URL}/register` , requestBody);
+      console.log( requestBody)
       return response.data;
     }catch( error) {
       console.log( "error registering data " +  error);
@@ -17,9 +18,5 @@ import axios from "axios"
 
  }
 
-// Default export for the service
-const authService = {
-  registerUser
-};
 
-export default authService;
+
