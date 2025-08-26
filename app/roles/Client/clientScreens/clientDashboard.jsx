124b90dff1cb565cbc2b1ab1b5f6ca1b60d314b3
randiranirman <randiranirman20011210@gmail.com>
2025-08-26 11:07:18 +0530

    fixed the routing isseu

---
+++ app/roles/Client/clientScreens/clientDashboard.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native"
+
+
+const ClientDashboard =() => {
+
+
+
+    return(
+
+        <View>
+
+            <Text> This is a sample</Text>
+        </View>
+    )
+}
+
+export default ClientDashboard;
--- app/services/authService.js
+++ app/services/authService.js
@@ -1,22 +1,29 @@
-import axios from "axios"
-
- const API_URL =  " http://192.168.8.108:8080/api/auth"
-
-
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 
 
- export const  registerUser =  async ( requestBody ) => { 
+const API_URL = "http://192.168.8.108:8080/api/auth";
 
-    try  { 
-      const response  = await axios.post(`${API_URL}/register` , requestBody);
-      console.log( requestBody)
-      return response.data;
-    }catch( error) {
-      console.log( "error registering data " +  error);
-      throw error;
+export const registerUser = async (requestBody) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, requestBody);
+
+    // Save token if available
+    if (response.data?.accessToken) {
+      await AsyncStorage.setItem("accessToken", response.data.accessToken);
     }
 
- }
+    // Check status
+    if (response.status === 200) {
+      router.push( '../roles/Client/clientScreens/clientDashboard');
+    }
 
+    console.log("Registered:", requestBody);
+    return response.data;
 
-
+  } catch (error) {
+    console.log("Error registering data:", error);
+    throw error;
+  }
+};
