a910677b05f31dcba5a090550d5cebe546c696ef
randiranirman <randiranirman20011210@gmail.com>
2025-09-04 21:42:41 +0530

    added traier screen for assign maels and workout

---
+++ app/roles/Trainer/trainerScreens/assignMealsAndWorkout.jsx
@@ -0,0 +1,23 @@
+
+
+import { View ,  Text } from "react-native"
+
+
+const AssignMealPlansAndWorkout =() => {
+
+
+
+    return (
+
+        <View>
+
+            <Text> This is   the screen for the meal assign for a spefic client </Text>
+        </View>
+    )
+
+
+
+
+}
+
+export default AssignMealPlansAndWorkout;
---
+++ app/roles/Trainer/trainerScreens/assignWorkout.jsx
@@ -0,0 +1,21 @@
+import { View,Text } from "react-native"
+
+
+
+const AssignWorkout =( ) => {
+
+
+    return (
+        
+
+        <View>
+
+            <Text>THis is the screen for assign workout   plans </Text>
+        </View>
+
+
+    )
+}
+
+export default AssignWorkout;
+
